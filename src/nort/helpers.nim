7e8be824b14de17c48842fd809a9cc7108db4de0
Jake Leahy <jake@leahy.dev>
2026-03-09 17:05:06 +1100

    Add whitespace also

---
+++ src/nort/helpers.nim
@@ -0,0 +1,11 @@
+## This contains pre-built grammars that you might find helpful
+
+import ./combinators
+
+import std/strutils
+
+let
+  nl*: Combinator[Void] = ?e('\r') * e('\n')
+    ## Newline character. Is cross platform and handles both windows and linux line endings
+
+  ws*: Combinator[Void] = e(Whitespace)
